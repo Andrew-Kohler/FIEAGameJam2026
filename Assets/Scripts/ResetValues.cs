4680d9fa17c50875799e0dcefd92d83488a96ec3
Gianna G <g.giliberto21@gmail.com>
2026-01-18 07:48:54 -0500

    Calculating final score

---
+++ Assets/Scripts/ResetValues.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ResetValues : MonoBehaviour
+{
+    public void Reset()
+    {
+        GameManager.Instance.ResetAllValues();
+    }
+
+    public void ReturnToStart()
+    {
+        SceneManager.LoadScene(2);
+    }
+}
